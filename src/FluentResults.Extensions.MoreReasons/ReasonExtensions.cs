970d95d8bd9861534afdf62c7539d2e3caab9c0b
pauldbentley <paul@paulbentley.co.uk>
2021-05-18 09:48:45 +0100

    Adds the GetMetadataOrDefault extension

---
+++ src/FluentResults.Extensions.MoreReasons/ReasonExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentResults
+{
+    public static class ReasonExtensions
+    {
+        public static TValue GetMetadataOrDefault<TValue>(this Reason reason, string key) =>
+            reason.HasMetadataKey(key)
+                ? (TValue)reason.Metadata[key]
+                : default;
+    }
+}
